6a772513c25ee3d58f4264c639261389b3428e37
匿名者475 <1059876295@qq.com>
2018-02-01 11:38:54 +0800

    修改pid_test文件

--- pid_test.py
+++ pid_test.py
@@ -2,13 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import psutil
+import sys
+
+params = sys.argv
+keyword = ''
+if params and len(params) > 1:
+    # 说明有参数
+    keyword = params[1]
 
 pids = psutil.pids()
 for pid in pids:
-    p = psutil.Process(pid).as_dict(attrs=['pid', 'name', 'username', 'exe', 'cwd', 'create_time'])
-    name = p["name"]
-    if 'python' not in name:
+    try:
+        p = psutil.Process(pid).as_dict(attrs=['pid', 'name', 'username', 'exe', 'cwd', 'create_time'])
+        name = p["name"] or ''
+        exe = p["exe"] or ''
+        cwd = p["cwd"] or ''
+        if keyword not in name and keyword not in exe and keyword not in cwd:
+            continue
+        print 'name: ', name
+        print 'exe: ', exe
+        print 'cwd: ', cwd
+    except Exception, e:
         continue
-    for keys in p.keys():
-        tempText = keys + ':' + str(p[keys]) + '\n'
-        print tempText
--- process_auto_run.sh
+++ process_auto_run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 date "+%Y-%m-%d %H:%M:%S"
 echo start check process
-echo $PYTHONPATH /home/gsma/monitor/conf_file.json
-python2.7 /home/gsma/monitor/process_auto_run.py
+echo $PYTHONPATH
+python2.7 /home/gsma/monitor/process_auto_run.py /home/gsma/monitor/conf_file.json
